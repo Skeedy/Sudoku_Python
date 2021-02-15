5a1d8632f848d975dd0e60bd4bf2f0c9bec517a5
Skeedy <pierre_tisserand@orange.fr>
2021-02-15 12:00:24 +0100

    first commit

---
+++ sudoku.py
@@ -0,0 +1,198 @@
+from tkinter import *
+from random import *
+
+class case:
+    def __init__(self, value, x, y, row, col, mainBlock, isShow = False):
+        self.value = value
+        self.coordonate = (row, col, mainBlock)
+        self.isShow = isShow
+        self.x = x + 49
+        self.y = y + 49
+    def createCase(self):
+        if self.isShow:
+            placeholder = StringVar()
+            placeholder.set(self.value)
+            self.input = Entry(window, justify='center', state='disabled', font=("Purisa", 30), textvariable = placeholder)
+        else:
+            self.input = Entry(window, justify='center', state='normal', font=("Purisa", 30))
+        self.canvas = canvas.create_window(self.x, self.y, window = self.input, height=99, width=99 )
+        blocks[self.coordonate] = self
+    def deleteCase(self):
+        canvas.delete(self.canvas)
+    def correction(self):
+        if self.input.get():
+            if self.value == int(self.input.get()):
+                self.input['fg'] = 'green'
+            if self.value != int(self.input.get()):
+                self.input['fg'] = 'red'
+        else:
+            pass
+    def __del__(self):
+        print(self.coordonate, "deleted")
+
+class buttonDifficulty:
+    def __init__(self, window, value, text):
+        self.value = value
+        self.text = text
+        self.window = window
+    def createButton(self):
+        self.button = Button(self.window, text=self.text )
+        self.button['command'] = lambda:createSudoko(self.value)
+        self.button.pack()
+    def deleteButton(self):
+        print('deltedbutton')
+        self.button.destroy()
+
+def createBigblock():
+    x = 0
+    y = 0
+    i = 0
+    while i < 9:
+        i = i + 1
+        canvas.tag_raise(canvas.create_rectangle(x, y, x+300, y+300, width=6))
+        x = x + 300
+        if i%3 == 0:
+            x = 0
+            y = y + 300
+
+def pickNumber(row, col, mainBlock):
+    fullList = [1,2,3,4,5,6,7,8,9]
+    newList = []
+    rowList = getBlocks(row, 'row')
+    #print('rowlist', rowList)
+    colList = getBlocks(col, 'col')
+    #print('colList', colList)
+    mainBlockList = getBlocks(mainBlock, 'mainBlock')
+    #print('mainBlockList', mainBlockList)
+    newList.extend(rowList)
+    newList.extend(colList)
+    newList.extend(mainBlockList)
+    newArray = []
+    for number in fullList:
+        if number not in newList:
+            newArray.append(number)
+    if len(newArray) > 0:
+        randomNumber = newArray[randrange(0,len(newArray))]
+        return randomNumber
+    else:
+        return False
+
+def getMainBlock(row, col):
+    if row<3:
+        if col<3:
+            mainBlock = 0
+        if 3 <= col < 6:
+            mainBlock = 1
+        if col>=6:
+            mainBlock = 2
+    if 3 <= row < 6:
+        if col<3:
+            mainBlock = 3
+        if 3 <= col <6:
+            mainBlock = 4
+        if col>=6:
+            mainBlock = 5
+    if row>=6:
+        if col<3:
+            mainBlock = 6
+        if 3 <= col <6:
+            mainBlock = 7
+        if col>=6:
+            mainBlock = 8
+    return mainBlock
+
+def createCol(x, y, i):
+    for j in range(9): #col
+        mainBlock = getMainBlock(i, j)
+        number = pickNumber(i,j, mainBlock)
+        if number is False:
+           return False
+        else:
+            newCase = case(number, x, y, i ,j ,mainBlock, False)
+            newCase.createCase()
+            if (j+1)%3 == 0:
+                x = x + 103
+            else:
+                x = x + 99
+
+def createRow(y, i = 0):
+    while i < 9 : #row
+        x = 0
+        if createCol(x, y, i) == False:
+            #print('row', actualRow)
+            deleteRows(i)
+            #print('row', actualRow)
+        else:
+            if (i+1)%3 == 0:
+                y = y + 103
+            else:
+                y = y + 99
+            i = i + 1
+
+def getBlocks(index, type):
+    newArray = []
+    if type == 'row':
+        blocksList = [item for item in blocks if item[0] == index] #compréhension de liste
+    if type == 'col':
+        blocksList = [item for item in blocks if item[1] == index]
+    if type == 'mainBlock':
+        blocksList = [item for item in blocks if item[2] == index]
+    for i in range(len(blocksList)):
+        blockId = blocksList[i]
+        value = blocks[blockId].value
+        newArray.append(value)
+    return newArray
+
+def deleteRows(rowIndex):
+      #print(rowIndex)
+    rowBlocks = [item for item in blocks if item[0] == rowIndex]
+    for i in range(len(rowBlocks)):
+        blockId = rowBlocks[i]
+        block = blocks[blockId]
+        print('block', block)
+        block.deleteCase()
+        print('blockId', blockId)
+        del blocks[blockId]
+
+def createSudoko(difficultyLevel):
+    createBigblock()
+    y = 0
+    createRow(y)
+    hydgfd
+
+def correction():
+    for i in blocks.values():
+        i.correction()
+
+def getDifficulty():
+    filewin = Toplevel(window)
+    buttonList = []
+    buttonNovice = buttonDifficulty(filewin, 1, 'Novice')
+    buttonNovice.createButton()
+    buttonNormal = buttonDifficulty(filewin, 2, 'Normal')
+    buttonNormal.createButton()
+    buttonExpert = buttonDifficulty(filewin, 3, 'Expert')
+    buttonExpert.createButton()
+
+blocks = {}
+window = Tk()
+
+menubar = Menu(window)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="New", command=createSudoko)
+filemenu.add_command(label="Difficulty", command=getDifficulty)
+
+menubar.add_cascade(label="File", menu=filemenu)
+
+
+
+difficutlyLevel = 0
+canvas = Canvas(window, height = 900, width = 900, bg = 'white')
+canvas.pack()
+correctionButton = Button(window,text='Correction', command=correction)
+correctionButton.pack()
+
+
+#canvas.create_window(50, 50, window=e1, height=100, width=100, state='disabled')
+window.config(menu=menubar)
+window.mainloop()
